f5f5c9833b36c130737d74489a562e1df5c6b0ad
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-05 20:31:07 +0530

    basic api setup done

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import serverConfig from "./config/server.config";
+import apiRouter from "./routes";
 
 const app = express();
+
+app.use("/api", apiRouter);
+
 app.listen(serverConfig.port, () => {
   console.log(`Server started on port ${serverConfig.port}`);
 });
