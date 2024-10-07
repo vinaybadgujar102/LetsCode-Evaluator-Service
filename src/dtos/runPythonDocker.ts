6ea2aa4fe94350fc2934c2caef5ba823835efa25
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-07 13:56:13 +0530

    log the buffer from the python docker container

--- src/dtos/runPythonDocker.ts
+++ src/dtos/runPythonDocker.ts
@@ -2,7 +2,8 @@ import createContainer from "../containers/containerFactory";
 
 async function runPython(code: string) {
   console.log("Initialising a new python container");
-
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const rawLogBuffer: any[] = [];
   const pythonDockerContainer = await createContainer("python:3.8-slim", [
     "python",
     "-c",
@@ -14,6 +15,21 @@ async function runPython(code: string) {
   await pythonDockerContainer.start();
 
   console.log("Container started");
+
+  const loggerStream = await pythonDockerContainer.logs({
+    stdout: true,
+    stderr: true,
+    timestamps: false,
+    follow: true, // whether the logs are streamed or returned as a string
+  });
+
+  loggerStream.on("data", (chunk) => {
+    rawLogBuffer.push(chunk);
+  });
+
+  loggerStream.on("end", () => {
+    console.log(rawLogBuffer);
+  });
 
   return pythonDockerContainer;
 }
