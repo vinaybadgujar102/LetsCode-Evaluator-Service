97d62b21b3da135691a8ae8efdd87c2a92892684
Vinay Badgujar <vinaybadgujar8@gmail.com>
2024-10-21 21:18:48 +0530

    fix: import issue

--- src/producers/evaluatorQueueProducer.ts
+++ src/producers/evaluatorQueueProducer.ts
@@ -1,4 +1,4 @@
-import EvaluationQueue from "../queues/EvaluationQueue";
+import EvaluationQueue from "../queues/evaluationQueue";
 
 export default async function (payload: Record<string, unknown>) {
   await EvaluationQueue.add("EvaluationJob", payload);
